6fcea1c337fa138e99a3fbb09d73d7d820bf9ff8
Eneko <Eneko@LAPTOP-DGT97667>
2020-02-20 20:05:01 +0100

    Library

--- src/Library.java
+++ src/Library.java
@@ -1,4 +1,14 @@
+import java.util.*;
 
 public class Library {
-
+	HashSet<String> books;
+	int time;
+	int libros_escaneados;
+	
+	public Library (HashSet<String> pBooks, int pTime , int pLibros_escaneados){
+		this.books = pBooks;
+		this.time = pTime;
+		this.libros_escaneados = pLibros_escaneados;
+	}
+	
 }
