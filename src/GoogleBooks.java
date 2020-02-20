e9d1724d1e026ac755cb02a27ed769b259623056
Eneko <Eneko@LAPTOP-DGT97667>
2020-02-20 20:27:32 +0100

    GoogleBooks

--- src/GoogleBooks.java
+++ src/GoogleBooks.java
@@ -1,4 +1,34 @@
+import java.util.*;
 
 public class GoogleBooks {
-
+	int MaxDias;
+	HashSet<Book> librosPorExaminar;
+	HashSet<Library> librerias;
+	private static GoogleBooks miGoogleBooks;
+	
+	private GoogleBooks () {
+		
+	}
+	public void addBook (Book pLibro){
+		if(librosPorExaminar.contains(pLibro)){
+		}
+		else{librosPorExaminar.add(pLibro);
+		}
+		
+	}
+	public static GoogleBooks getMiGoogleBooks(){
+		if (miGoogleBooks == null){
+			miGoogleBooks = new GoogleBooks();
+		}
+		return miGoogleBooks;
+	}
+	public void setMaxDias(int pMaxDias){
+		this.MaxDias=pMaxDias;
+	}
+	public void addLibrerias(Library pLibreria){
+		if(librerias.contains(pLibreria)){
+		}
+		else{librerias.add(pLibreria);
+		}
+	}
 }
