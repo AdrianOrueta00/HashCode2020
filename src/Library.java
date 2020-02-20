657680023d6be1ccd4d648952e72255e46f696cb
AdrianOrueta00 <48363332+AdrianOrueta00@users.noreply.github.com>
2020-02-20 21:00:38 +0100

    Update Library.java
    Corregidos tipos de atributos y cabeceras de métodos.
    Implementado getLibrosPosibles()

--- src/Library.java
+++ src/Library.java
@@ -1,14 +1,28 @@
 import java.util.*;
 
 public class Library {
-	HashSet<String> books;
+	Book[] books;
 	int time;
-	int libros_escaneados;
+	int librosPorDia;
 	
-	public Library (HashSet<String> pBooks, int pTime , int pLibros_escaneados){
+	public Library (Book[] pBooks, int pTime , int pLibros_escaneados){
 		this.books = pBooks;
 		this.time = pTime;
-		this.libros_escaneados = pLibros_escaneados;
+		this.librosPorDia = pLibros_escaneados;
 	}
-	
+	 public Book[] getLibrosPosibles(int pTRestante){
+		 int t=pTRestante;
+		 int nEnviados=0;
+		 Book[] l = this.books;
+		 Book[] r = new Book[books.length];
+		 while (t>0 && nEnviados<(l.length-1)){
+			 for (int i=0;i<librosPorDia;i++){
+				 if (nEnviados<(l.length-1)){
+					 nEnviados++;
+					 r[nEnviados+i]=l[nEnviados+i];
+				 }
+			 }
+		 }
+		 return r;
+	 }
 }
